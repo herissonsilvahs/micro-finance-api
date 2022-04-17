6d431560399b909435faf4286bd663ffb6301941
Herisson Silva <herissonsilvahs@gmail.com>
2022-04-17 11:01:38 -0300

    reconfig

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import MongoDBService from './integrations/MongoDBService'
+import routes from './app.routes'
+// import './db/models/Account';
+// import './db/models/Institution';
+
+dotenv.config({
+  debug: !process.env.PRODUCTION
+})
 
 const PORT = process.env.PORT || 3000
 
@@ -11,4 +20,10 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(PORT)
+app.use('/v1', routes)
+
+MongoDBService.connect()
+  .then(() => console.log('DB Connected!'))
+  .catch(error => console.log('error: ', error))
+
+app.listen(PORT, () => console.log(`Server up at port: ${PORT}`))
